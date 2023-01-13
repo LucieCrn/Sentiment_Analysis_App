1f35f472de01fbfbc5009807c1ec9076b5b212b2
LucieCrn <101023427+LucieCrn@users.noreply.github.com>
2023-01-13 15:30:52 +0100

    Update sentiment_analysis_app.py

--- sentiment_analysis_app.py
+++ sentiment_analysis_app.py
@@ -49,44 +49,46 @@ def analyze_token_sentiment(docx):
 def main():
 	st.title("Sentiment Analysis App")
 
+		
+	with st.form(key='nlpForm'):
+		raw_text = st.text_area("Enter Text Here")
+		submit_button = st.form_submit_button(label='Analyze')
+
 	# layout
 	col1,col2 = st.columns(2)
-	
-	with st.form(key='nlpForm'):
-	raw_text = st.text_area("Enter Text Here")
-	submit_button = st.form_submit_button(label='Analyze')
+	if submit_button:
 
-	with col1:
-		st.info("Results")
-		sentiment = TextBlob(raw_text).sentiment
-		st.write(sentiment)
+		with col1:
+			st.info("Results")
+			sentiment = TextBlob(raw_text).sentiment
+			st.write(sentiment)
 
-		# Emoji
-		if sentiment.polarity > 0.2:
-			st.markdown("Sentiment:: Positive :smiley: ")
-		elif sentiment.polarity < 0.2:
-			st.markdown("Sentiment:: Negative :angry: ")
-		else:
-			st.markdown("Sentiment:: Neutral 😐 ")
+			# Emoji
+			if sentiment.polarity > 0:
+				st.markdown("Sentiment:: Positive :smiley: ")
+			elif sentiment.polarity < 0:
+				st.markdown("Sentiment:: Negative :angry: ")
+			else:
+				st.markdown("Sentiment:: Neutral 😐 ")
 
-		# Dataframe
-		result_df = convert_to_df(sentiment)
-		st.dataframe(result_df)
+			# Dataframe
+			result_df = convert_to_df(sentiment)
+			st.dataframe(result_df)
 
-		# Visualization
-		c = alt.Chart(result_df).mark_bar().encode(
-			x='metric',
-			y='value',
-			color='metric')
-		st.altair_chart(c,use_container_width=True)
+			# Visualization
+			c = alt.Chart(result_df).mark_bar().encode(
+				x='metric',
+				y='value',
+				color='metric')
+			st.altair_chart(c,use_container_width=True)
 
 
 
-	with col2:
-		st.info("Token Sentiment")
+		with col2:
+			st.info("Token Sentiment")
 
-		token_sentiments = analyze_token_sentiment(raw_text)
-		st.write(token_sentiments)
+			token_sentiments = analyze_token_sentiment(raw_text)
+			st.write(token_sentiments)
 
 
 
